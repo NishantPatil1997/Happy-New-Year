558f0a0c8c73adead87efd73e645c257ca32c907
NishantPatil1997 <patilnishant750@gmail.com>
2024-12-28 12:14:56 +0530

    Add files via upload

---
+++ Nishant.py
@@ -0,0 +1,8 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:/Users/VOCATIONAL LAB/Desktop/Nishant.py =============
+c
+>>> 
+============== RESTART: C:/Users/VOCATIONAL LAB/Desktop/Nishant.py =============
+Happy New Yaer
